8aad75e29fefb93866e523bfb8c602cbcd79220f
webB1an <webB1anyaoyao@gmail.com>
2024-10-10 17:21:27 +0800

    feat(views): 添加数据

--- src/views/tools/index.vue
+++ src/views/tools/index.vue
@@ -39,13 +39,13 @@ const link = [
     text: "剧集区",
     children: [
       {
-        id: 1,
+        id: 1.1,
         from: "剧集区",
         text: "国产",
         link: "/detail/list/series/zh"
       },
       {
-        id: 2,
+        id: 1.2,
         from: "剧集区",
         text: "美国",
         link: "/detail/list/series/en"
@@ -56,16 +56,53 @@ const link = [
     text: "电影区",
     children: [
       {
-        id: 3,
+        id: 2.1,
         from: "电影区",
         text: "国产",
         link: "/detail/list/movie/zh"
       },
       {
-        id: 4,
+        id: 2.2,
         from: "电影区",
         text: "美国",
-        link: "/detail/list/movie/en"
+        // link: "/detail/list/movie/en"
+        link: "https://pan.quark.cn/s/94a36f5c74e6"
+      },
+      {
+        id: 2.3,
+        from: "电影区",
+        text: "系列作品",
+        link: "https://pan.quark.cn/s/6cb02baa39f5"
+      },
+      {
+        id: 2.4,
+        from: "电影区",
+        text: "玛丽苏合集",
+        link: "https://pan.quark.cn/s/6dd2fcb6d021"
+      },
+      {
+        id: 2.5,
+        from: "电影区",
+        text: "🔞疼痛的爱系列",
+        link: "https://ai-compass.github.io/resource-sharing/#/detail/bdsm"
+      },
+      {
+        id: 2.6,
+        from: "电影区",
+        text: "欧美大尺度系列",
+        link: "https://pan.quark.cn/s/3130f810982c"
+      },
+      {
+        id: 2.7,
+        from: "电影区",
+        text: "为艺术献身作品",
+        link: "https://pan.quark.cn/s/319e152026b8"
+      },
+      {
+        id: 2.8,
+        from: "电影区",
+        text: "豆瓣 TOP250",
+        link: "https://pan.quark.cn/s/1bbfb6676b01"
       }
     ]
   },
@@ -110,7 +147,7 @@ const link = [
         id: 9,
         from: "其他区",
         text: "动态壁纸",
-        link: "https://pan.quark.cn/s/71056a75dc66"
+        link: "https://pan.quark.cn/s/957838f63ac6"
       }
     ]
   }
